31f30749fa9ceeff689080d0dbdae6234f6c1761
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-14 15:47:00 -0700

    initial

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Note = () => {
+    return <div className='note'>
+            <h1>The Note Title</h1>
+            <p>This is some content</p>
+    </div>
+}
+
+export default Note;

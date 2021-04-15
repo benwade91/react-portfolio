b3badf1b160fdfe78e82fa7d3e37c1b6498ae7ef
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-15 09:03:28 -0700

    fixed links to images in projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import simpleSuds from "../images/SimpleSuds.png";
+import iCycle from "../images/icycle.png";
+import employeeManager from "../images/employeemngr.png";
+import noteTaker from "../images/notetaker.png";
+import weatherDash from "../images/weather.png";
 
 const Project = () => {
     return (
@@ -9,35 +14,35 @@ const Project = () => {
         <div className="theGrid">
             <div className="gridItem1 gridItem">
                 <a href="https://simple-suds.herokuapp.com/">
-                    <img src="./assets/newPhotos/SimpleSuds.png" alt="SimpleSuds" />
+                    <img src={simpleSuds} alt="SimpleSuds" />
                     <p>SimpleSuds</p>
                 </a>
                 <label htmlFor="SimpleSuds">HandlebarsJS, HTML, CSS, Bootstrap</label>
             </div>
             <div className="gridItem2 gridItem">
                 <a href="https://ohwhytina.github.io/iCycle/">
-                    <img src="./assets/newPhotos/icycle.png" alt="iCycle" />
+                    <img src={iCycle} alt="iCycle" />
                     <p>iCycle</p>
                 </a>
                 <label htmlFor="iCycle">GoogleMaps API, JavaScript, jQuery</label>
             </div>
             <div className="gridItem3 gridItem">
                 <a href="https://github.com/benwade91/Employee-Manager">
-                    <img src="./assets/newPhotos/employeemngr.png" alt="Employee Manager" />
+                    <img src={employeeManager} alt="Employee Manager" />
                     <p>Employee Manager</p>
                 </a>
                 <label htmlFor="Employee Manager">InquirerJS, Mysql2, Node</label>
             </div>
             <div className="gridItem4 gridItem">
                 <a href="https://my-notes-1025.herokuapp.com/">
-                    <img src="./assets/newPhotos/notetaker.png" alt="Note Taker" />
+                    <img src={noteTaker} alt="Note Taker" />
                     <p>Note Taker</p>
                 </a>
                 <label htmlFor="Note Taker">EspressJS, JavaScript</label>
             </div>
             <div className="gridItem5 gridItem">
                 <a href="https://benwade91.github.io/Weather-Dashboard/">
-                    <img src="./assets/newPhotos/weather.png" alt="Weather Dashboard" />
+                    <img src={weatherDash} alt="Weather Dashboard" />
                     <p>Weather Dashboard</p>
                 </a>
                 <label htmlFor="Weather Dashboard">WeatherAPI, JavaScript</label>

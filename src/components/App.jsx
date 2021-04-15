646586c1d81af7892005bb915ab6415898d49916
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-15 09:15:21 -0700

    connected css and images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Hero';
 import About from './About';
 import Project from './Project';
 import Contact from './Contact';
+import '../CSS/styles.css';
 
 let App = () =>{
     return (<div><Header /><Hero /><About /><Project /><Contact /></div>)

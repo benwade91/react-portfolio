af0a5d0c6e98111872d331bb2e6fb799235d968c
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-23 19:27:47 -0600

    downloadable pdf

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,6 @@ import './style.css'
 const Footer = () => {
 
     return <footer>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <div className="copyright">
             <p>Thanks for checking me out!</p>
         </div>
--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './style.css'
 const Resume = () => {
     return <div className='resumeBody'>
+        <a href="https://docs.google.com/document/d/1s2n5qTbV2ZZS1qKAZmlodEvU8re8H9vXfvWUXXDVLgA/export?format=pdf" className='fa fa-download fa-3x dlLink' download> </a>
         <iframe title='resume' src="https://docs.google.com/document/d/e/2PACX-1vSELbG2XkU8VGcopmlH20FPsoHQYuypq4Ojb0SdCaWmpletgWKmtlxZo6pncs0UrBJMsk4Q4a-UFuhj/pub?embedded=true"></iframe>
+  
     </div>
 }
 

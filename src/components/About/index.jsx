bf74e048c33f231718d78010f5b3e96dd1017093
benwade91 <70988204+benn925@users.noreply.github.com>
2021-05-11 11:35:28 -0700

    updated about me and contact form

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import selfie from '../../images/IMG_1394.jpeg';
+import './style.css';
+
 
 const Hero = () => {
     return (
@@ -9,7 +11,37 @@ const Hero = () => {
                     <img className="" src={selfie} alt="A beautiful persons face." />
                 </div>
                 <div className="words">
-                    <p>Based in the San Francisco Bay Area, I'm a full stack web developer with a passion for front end development. Proficient with HTML, CSS, JavaScript, NodeJS, Jquery, Express, ReactJS, Git, GitHub SQL, NoSQL, MySQL, Mongoose, MongoDB, Bootstrap, HandlebarsJS, Webpack and more...</p>
+                    <div className='aboutSection'>
+                        <div className='aboutPod'>
+                            <h3>About Me</h3>
+                            <p>My name is Ben and I'm a full stack web developer living in the San Francisco Bay Area. I enjoy building quality web apps, big and small. I'm passionate about producing work with great UI/UX and mobile responsiveness. If you are a small business looking for a website, or a business looking to hire, please reach out!</p>
+                        </div>
+                        <div className='aboutPod'>
+                            <h3>Skills</h3>
+                            <img src="images/html5.png" alt=""/>
+                            <ul>
+                                <li>MongoDB</li>
+                                <li>Express</li>
+                                <li>React </li>
+                                <li>Node</li>
+                                <li>JavaScipt</li>
+                                <li>CSS/SASS</li>
+                                <li>GraphQL</li>
+                                <li>Webpack</li>
+                                <li>JQuery</li>
+                                <li>Bootstrap</li>
+                                <li>Handlebars</li>
+                                <li>MySQL </li>
+                                <li>Git</li>
+
+                            </ul>
+                        </div>
+                        <div className='aboutPod'>
+                            <h3>Who am I?</h3>
+                            <p>I'm a person who is perpetually trying to learn something new. In my free time I enjoy reading about and experimenting with new frameworks and technologies. I love the feeling I get when i finally find that bug that's been eluding me for the last few hours. Away from the computer, I enjoy working on my car, spending time outdoors, and training JiuJitsu. </p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>)
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style.css';
 
 let Contact = () => {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const [errorMessage, setErrorMessage] = useState('');
-    const { name, email, message } = formState;
+    // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // const [errorMessage, setErrorMessage] = useState('');
+    // const { name, email, message } = formState;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!errorMessage) {
-            window.location.href = `mailto:benwade91@gmail.com?subject=New Contact from ${formState.name}&body=${formState.message}`
-        }
-    };
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     if (!errorMessage) {
+    //         window.location.href = `mailto:benwade91@gmail.com?subject=New Contact from ${formState.name}&body=${formState.message}`
+    //     }
+    // };
 
-    const handleChange = (e) => {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            if (!isValid) {
-                setErrorMessage(`What's up with the email?`);
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`Please leave me a ${e.target.name}!`);
-            } else {
-                setErrorMessage('');
-            }
-        }
-        if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-    };
+    // const handleChange = (e) => {
+    //     if (e.target.name === 'email') {
+    //         // const isValid = validateEmail(e.target.value);
+    //         // if (!isValid) {
+    //         //     setErrorMessage(`What's up with the email?`);
+    //         // } else {
+    //         //     setErrorMessage('');
+    //         // }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`Please leave a ${e.target.name}!`);
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    //     if (!errorMessage) {
+    //   setFormState({ ...formState, [e.target.name]: e.target.value });
+    // }
+    // };
 
-    function validateEmail(email) {
-        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
+    // function validateEmail(email) {
+    //     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     return re.test(String(email).toLowerCase());
+    // }
 
     return (
         <section id="contact-me" className="content">
@@ -44,7 +44,7 @@ let Contact = () => {
                 <h3>Contact Me</h3>
             </div>
             <div className="words">
-                <form id="contact-form" onSubmit={handleSubmit}>
+                {/* <form id="contact-form" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="name">Name:</label><br/>
                         <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
@@ -63,7 +63,14 @@ let Contact = () => {
                         </div>
                     )}
                     <button className='submitBtn' data-testid="button" type="submit">Submit</button>
-                </form>
+                </form> */}
+
+                <div className='contactBox'>
+                    <a href="mailto:Ben@BenNeedsAJob.com" >Email <span className='fa fa-envelope'></span></a>
+                    <a href="https://github.com/benwade91" >GitHub <span className='fa fa-github'></span></a>
+                    <a href="https://www.linkedin.com/in/ben-wade-6abb621b7/" >LinkedIn <span className='fa fa-linkedin'></span></a>
+                    <a href="https://www.instagram.com/_benwade_/" >Instagram <span className='fa fa-instagram'></span></a>
+                </div>
             </div>
         </section>
     );
